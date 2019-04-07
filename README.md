40f9ff72a82da1a8d82c126f39c4cfd3c8736726
Isar Ramses (r) <rammses_@live.com.mx>
2019-04-06 23:45:49 -0600

    How does it work?

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # graplql
 Test GraphQL with Nodejs and MongoDB
+
+## How does it work?
+Requires Node.js 10
+
+* `npm install` to install the dependencies.
+* `npm run build` to build with Babel.
+* `npm run clean` to delete dist/.
+* `npm run dev` to run in the development environment.
+
+## License
+MIT
--- dist/index.js
+++ dist/index.js
@@ -2,10 +2,30 @@
 
 var _express = _interopRequireDefault(require("express"));
 
+var _expressGraphql = _interopRequireDefault(require("express-graphql"));
+
+var _schema = _interopRequireDefault(require("./schema"));
+
+var _database = require("./database");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var app = (0, _express["default"])();
+(0, _database.connect)();
+app.get('/', function (req, res) {
+  res.json({
+    message: 'Hello world'
+  });
+});
+app.use('/graphql', (0, _expressGraphql["default"])({
+  graphiql: true,
+  schema: _schema["default"],
+  context: {
+    messageId: 'test',
+    other: 'blabla'
+  }
+}));
 app.listen(3000, function () {
-  return console.log('Server on port 3000');
+  return console.log("\uF898 Server on port 3000");
 });
 //# sourceMappingURL=index.js.map
